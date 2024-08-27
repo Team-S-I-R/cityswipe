d47c8682fd8d725a0d7c58a24b825ccfc53e6c8e
Itwela Ibomu <36617380+itwela@users.noreply.github.com>
2024-08-27 18:30:34 -0400

    images added to sidebar working on ui now

--- cityswipe-fe/app/cs-componets/sidebar.tsx
+++ cityswipe-fe/app/cs-componets/sidebar.tsx
@@ -66,53 +66,53 @@ export default function Sidebar() {
 
     
     return (
-        <div className="w-full h-full" > 
+        <div className="w-full h-full flex flex-col gap-6" > 
         
             <div className="w-full h-[15%]">
 
-            <div className="w-full flex place-content-end h-max bg-gradient-to-t from-cyan-500 to-green-400 p-5">
-                <div className="w-1/2 flex place-content-start place-items-center justify-start h-full gap-5">
-                    <Link onClick={reload} href="/">
-                        <div className="rounded-full flex place-items-center place-content-center p-2 cursor-pointer bg-white w-10 h-10">
-                        {/* avatar/profile will go here */}
-                            <House  className="text-primary/50" size={15} />
-                        </div>
-                    </Link>
-                </div>
-                <div className="w-1/2 flex place-content-start place-items-center justify-end h-full gap-5">
+                <div className="w-full flex place-content-end h-max bg-gradient-to-t from-cyan-500 to-green-400 p-5">
+                    <div className="w-1/2 flex place-content-start place-items-center justify-start h-full gap-5">
+                        <Link onClick={reload} href="/">
+                            <div className="rounded-full flex place-items-center place-content-center p-2 cursor-pointer bg-white w-10 h-10">
+                            {/* avatar/profile will go here */}
+                                <House  className="text-primary/50" size={15} />
+                            </div>
+                        </Link>
+                    </div>
+                    <div className="w-1/2 flex place-content-start place-items-center justify-end h-full gap-5">
+                            
+                            {/* chat icon */}
+                            <TooltipProvider>
+                                <Tooltip>
+                                    <TooltipTrigger>
+                                        <div className="w-max h-max rounded-full p-2 cursor-pointer hover:scale-125">
+                                            <MessageCircleMore size={15} onClick={() => handleChatting()} className="text-white"/>
+                                        </div>
+                                    </TooltipTrigger>
+                                    <TooltipContent>
+                                    <p>Chat</p>
+                                    </TooltipContent>
+                                </Tooltip>
+                            </TooltipProvider>
                         
-                        {/* chat icon */}
-                         <TooltipProvider>
-                            <Tooltip>
-                                <TooltipTrigger>
-                                    <div className="w-max h-max rounded-full p-2 cursor-pointer hover:scale-125">
-                                        <MessageCircleMore size={15} onClick={() => handleChatting()} className="text-white"/>
-                                    </div>
-                                </TooltipTrigger>
-                                <TooltipContent>
-                                <p>Chat</p>
-                                </TooltipContent>
-                            </Tooltip>
-                        </TooltipProvider>
-                       
-                       {/* match icon */}
-                        <TooltipProvider>
-                            <Tooltip>
-                                <TooltipTrigger>
-                                    <div className="w-max h-max rounded-full p-2 cursor-pointer hover:scale-125">
-                                    <MessageCircleHeart size={15} onClick={() => handleMatching()} className="text-white"/>
-                                    </div>
-                                </TooltipTrigger>
-                                <TooltipContent>
-                                <p>Match</p>
-                                </TooltipContent>
-                            </Tooltip>
-                        </TooltipProvider>
+                        {/* match icon */}
+                            <TooltipProvider>
+                                <Tooltip>
+                                    <TooltipTrigger>
+                                        <div className="w-max h-max rounded-full p-2 cursor-pointer hover:scale-125">
+                                        <MessageCircleHeart size={15} onClick={() => handleMatching()} className="text-white"/>
+                                        </div>
+                                    </TooltipTrigger>
+                                    <TooltipContent>
+                                    <p>Match</p>
+                                    </TooltipContent>
+                                </Tooltip>
+                            </TooltipProvider>
+                    </div>
+
                 </div>
 
-            </div>
-
-            <p className="p-5">Matches</p>
+                <p className="p-5">Matches</p>
 
             </div>
 
@@ -121,23 +121,27 @@ export default function Sidebar() {
 
             <div className="w-full h-[80%] no-scrollbar overflow-y-scroll">
 
-            <div className="w-full h-[10%]"></div>
-            {destination.destinations.map((dest: DestinationItem) => {
-                const { cityAndCountry } = extractMatchInfo(dest.location);
-                return (
-                    <>
-                    <div onClick={() => handleCityMatch(cityAndCountry, dest.illustration ?? '')} className="w-full hover:bg-slate-300/20 flex flex-col gap-[15px] place-items-start">
-                        <div className="w-full relative flex flex-col p-5" 
-                        // style={{ backgroundImage: `url(${dest.illustration})`, backgroundRepeat: 'no-repeat', backgroundSize: 'fill', backgroundPosition: 'center' }}
-                        key={dest.id}>
-                            <img className="absolute inset-0 h-full w-full object-cover z-[-1]" src={dest.illustration} alt="" />
-                            <h3 className="text-2xl font-bold">{cityAndCountry}</h3>
-                            <p>Rating: {dest.rating}%</p>
+                {destination.destinations.map((dest: DestinationItem) => {
+                    const { cityAndCountry } = extractMatchInfo(dest.location);
+                    return (
+                        <>
+                        <div onClick={() => handleCityMatch(cityAndCountry, dest.illustration ?? '')} className="w-full h-max flex flex-col gap-[15px] overflow-hidden place-items-start">
+                            <div className="w-full h-[110px] cursor-pointer relative flex flex-col place-items-start place-content-center" 
+                            // style={{ backgroundImage: `url(${dest.illustration})`, backgroundRepeat: 'no-repeat', backgroundSize: 'fill', backgroundPosition: 'center' }}
+                            key={dest.id}>
+                                <div className="absolute w-full h-full flex place-items-end place-content-end">
+                                    {/* <div className="absolute z-[-1] bg-gradient-to-r from-white via-white to-transparent w-full h-full"></div> */}
+                                    <img className="relative z-[-2] w-1/2 inset-0 h-full w-full object-cover z-[-1]" src={dest.illustration} alt="" />
+                                </div>
+                                <div className="p-5 text-white">
+                                    <h3 className="text-md font-bold">{cityAndCountry}</h3>
+                                    <p className="text-sm">Rating: {dest.rating}%</p>
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                    </>
-                );
-            })}
+                        </>
+                    );
+                })}
 
             </div>
 
