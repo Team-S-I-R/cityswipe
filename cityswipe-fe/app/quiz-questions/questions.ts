24149d951ac570bc5a54e75eaca1970215834e8b
Rehan Nagoor <rehannags@yahoo.com>
2024-09-02 22:36:32 +0400

    new questions json format

--- cityswipe-fe/app/quiz-questions/questions.ts
+++ cityswipe-fe/app/quiz-questions/questions.ts
@@ -1,4 +1,4 @@
-const quizQuestions = {
+const oldQuizQuestions = {
   question1: "Where are you currently living?",
   question2: "Are you looking for luxury, mid-range, or budget destinations?",
   question3: "Which language(s) do you speak fluently?",
@@ -12,5 +12,68 @@ const quizQuestions = {
   question11: "Are you looking for destinations with specific activities, health facilities, or services?",
   question12: "Are you comfortable traveling to a country where open recreational drug use is allowed?",
 };
+
+const quizQuestions = [
+  {
+    id: 1,
+    question: "Where are you currently living?",
+    answerOptions: ["custom"],
+  },
+  {
+    id: 2,
+    question: "Are you looking for luxury, mid-range, or budget destinations?",
+    answerOptions: ["custom"],
+  },
+  {
+    id: 3,
+    question: "Which language(s) do you speak fluently?",
+    answerOptions: ["custom"],
+  },
+  {
+    id: 4,
+    question: "Are you open to traveling to a country where the primary language is different from your native language?",
+    answerOptions: ["custom"],
+  },
+  {
+    id: 5,
+    question: "Which season would you prefer to travel in?",
+    answerOptions: ["custom"],
+  },
+  {
+    id: 6,
+    question: "Which climate preference do you have: warm, cold, or temperate?",
+    answerOptions: ["custom"],
+  },
+  {
+    id: 7,
+    question: "What type of destination appeals to you the most: beach, mountain, or city?",
+    answerOptions: ["custom"],
+  },
+  {
+    id: 8,
+    question: "List some of your favorite activities that you enjoy doing in your free time.",
+    answerOptions: ["custom"],
+  },
+  {
+    id: 9,
+    question: "Do you have any dietary restrictions or preferences?",
+    answerOptions: ["custom"],
+  },
+  {
+    id: 10,
+    question: "Are you interested in trying local cuisine, fine dining, or street food?",
+    answerOptions: ["custom"],
+  },
+  {
+    id: 11,
+    question: "Are you looking for destinations with specific activities, health facilities, or services?",
+    answerOptions: ["custom"],
+  },
+  {
+    id: 12,
+    question: "Are you comfortable traveling to a country where open recreational drug use is allowed?",
+    answerOptions: ["custom"],
+  },
+];
 
 export default quizQuestions;
