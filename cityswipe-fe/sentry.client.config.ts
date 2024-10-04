81114b6226cce700f9a82508ad7ece9be2370909
Muhammad Bilal Khan <lofi.audit@gmail.com>
2024-10-04 13:53:57 -0400

    add: sentry config, posthog config
    FIX REQUIRED: posthog configuration not working (entirely)!

---
+++ cityswipe-fe/sentry.client.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the client.
+// The config you add here will be used whenever a users loads a page in their browser.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://e1d6ba50a1c528624f3dc44be2eb9a39@o4508056549523456.ingest.us.sentry.io/4508056550572032",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
